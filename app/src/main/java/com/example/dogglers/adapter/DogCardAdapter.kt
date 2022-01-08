f73fd9ff7e3389a93b9d260f82f5d8edc62c92e3
IAmrinderSingh <amrindersingh1703@gmail.com>
2022-01-08 07:37:25 +0530

    Inflating layout according to layout selected

--- app/src/main/java/com/example/dogglers/adapter/DogCardAdapter.kt
+++ app/src/main/java/com/example/dogglers/adapter/DogCardAdapter.kt
@@ -16,12 +16,14 @@
 package com.example.dogglers.adapter
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.dogglers.R
+import com.example.dogglers.const.Layout
 import com.example.dogglers.data.DataSource
 
 /**
@@ -50,12 +52,15 @@ class DogCardAdapter(
         // TODO: Use a conditional to determine the layout type and set it accordingly.
         //  if the layout variable is Layout.GRID the grid list item should be used. Otherwise the
         //  the vertical/horizontal list item should be used.
-
-        // TODO Inflate the layout
+        val adapterLayout=when(layout){
+            // TODO Inflate the layout
+            Layout.GRID-> LayoutInflater.from(parent.context).inflate(R.layout.grid_list_item,parent,false)
+            else-> LayoutInflater.from(parent.context).inflate(R.layout.vertical_horizontal_list_item,parent,false)
+        }
 
         // TODO: Null should not be passed into the view holder. This should be updated to reflect
         //  the inflated layout.
-        return DogCardViewHolder(null)
+        return DogCardViewHolder(adapterLayout)
     }
 
     override fun getItemCount(): Int = 0 // TODO: return the size of the data set instead of 0
